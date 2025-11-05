c811f34dc23ec10511fdfd3dc44a55afd6be0518
Melissa Aquino <melissa.aquino.andrade@gmail.com>
2025-11-04 23:18:36 -0300

    fim do projetão super legal eu tô morrendo de sono

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button")
+button.addEventListener("click", receiveData)
+
+async function receiveData() {
+   const persons = await fetch("http://localhost:3333").then(response => response.json())
+   persons.map(person => {
+    document.querySelector("main").innerHTML +=`
+        <section>
+            <h2>Nome: ${person.name}</h2>
+            <p>E-mail: ${person.email}</p>
+            <p>Idade: ${person.age}</p>
+            <p>Apelido: ${person.nickname}</p>
+        </section>
+    `
+
+})
+}
